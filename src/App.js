4e9a3df1efa09173220124a8319ee97e54cf0cba
Vinitha-Thiagarajan <vinitha_thiagarajan@condenast.com>
2021-11-25 17:00:50 +0530

    roles_permission

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import { Route, Switch, Redirect } from "react-router-dom";
 
 export default function App() {
   return (
-    <Switch>
-      <Redirect exact from="/home" to="/home/about" />
-      <Route exact path="/home/:page?" render={props => <Home {...props} />} />
-    </Switch>
+    <Home/>
   );
 }
